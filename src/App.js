2b4b0ffd724e66cf10e822800672c8212468fe16
mharisqadri <mharisqadri@gmail.com>
2024-03-29 06:17:02 +0500

    buttons and features added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
 <div className="container my-3">
 <TextForm heading="Enter the text below to analyze" />
 </div>
+
 </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,13 +1,74 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    
+    const handleLoClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleOnChange = (event)=>{
+        setText(event.target.value)
+    }
+
+    const handleClrClick = ()=>{
+        let newText = "";
+        setText(newText)
+    }
+
+    const handleEmailClick = ()=>{
+        let newText =(text.match.email); //=== "@"); {
+        setText(newText)
+    }
+
+    const [text, setText] = useState("Enter Text Here")
     return (
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary">Convert to Uppercase</button>
+        <button className="btn btn-primary" onClick={handleClrClick}>Clear Text</button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleEmailClick}>Extract Email</button>
+        <br/>
+        <h2>Your Text Summary</h2>
+        <p>Word Count:{text.split(" ").length-1} and {text.length} Characters</p>
+    <p>Minutes To Read:{0.008*text.split("").length}</p>
+    <h2>Preview</h2>
+    {text}
     </div>
+
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
